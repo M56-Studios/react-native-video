73d6c4ab327558e036a9aa55d68939f72582a586
Igor Tironi <tironiigor@gmail.com>
2023-01-05 13:00:23 -0800

    Fix crash changing source too fast #2917

--- ios/Video/RCTVideo.swift
+++ ios/Video/RCTVideo.swift
@@ -213,81 +213,79 @@ class RCTVideo: UIView, RCTVideoPlayerViewControllerDelegate, RCTPlayerObserverH
             ])
         }
     }
-    
+
     // MARK: - Player and source
-    
     @objc
     func setSrc(_ source:NSDictionary!) {
-        _source = VideoSource(source)
-        removePlayerLayer()
-        _playerObserver.player = nil
-        _playerObserver.playerItem = nil
-        
-        // perform on next run loop, otherwise other passed react-props may not be set
-        RCTVideoUtils.delay()
-            .then{ [weak self] in
-                guard let self = self else {throw NSError(domain: "", code: 0, userInfo: nil)}
-                guard let source = self._source,
-                let assetResult = RCTVideoUtils.prepareAsset(source: source),
-                let asset = assetResult.asset,
-                let assetOptions = assetResult.assetOptions else {
-                      DebugLog("Could not find video URL in source '\(self._source)'")
-                      throw NSError(domain: "", code: 0, userInfo: nil)
-                  }
-                
-#if canImport(RCTVideoCache)
-                if self._videoCache.shouldCache(source:source, textTracks:self._textTracks) {
-                    return self._videoCache.playerItemForSourceUsingCache(uri: source.uri, assetOptions:assetOptions)
-                }
-#endif
-                
-                if self._drm != nil || self._localSourceEncryptionKeyScheme != nil {
-                    self._resouceLoaderDelegate = RCTResourceLoaderDelegate(
-                        asset: asset,
-                        drm: self._drm,
-                        localSourceEncryptionKeyScheme: self._localSourceEncryptionKeyScheme,
-                        onVideoError: self.onVideoError,
-                        onGetLicense: self.onGetLicense,
-                        reactTag: self.reactTag
-                    )
-                }
-                return Promise{self.playerItemPrepareText(asset: asset, assetOptions:assetOptions)}
-            }.then{[weak self] (playerItem:AVPlayerItem!) in
-                guard let self = self else {throw  NSError(domain: "", code: 0, userInfo: nil)}
-                
-                self._player?.pause()
-                self._playerItem = playerItem
-                self._playerObserver.playerItem = self._playerItem
-                self.setPreferredForwardBufferDuration(self._preferredForwardBufferDuration)
-                self.setFilter(self._filterName)
-                if let maxBitRate = self._maxBitRate {
-                    self._playerItem?.preferredPeakBitRate = Double(maxBitRate)
-                }
-                
-                self._player = self._player ?? AVPlayer()
-                DispatchQueue.global(qos: .default).async {
-                    self._player?.replaceCurrentItem(with: playerItem)
-                }
+        DispatchQueue.global(qos: .default).async {
+            _source = VideoSource(source)
+            removePlayerLayer()
+            _playerObserver.player = nil
+            _playerObserver.playerItem = nil
 
-                self._playerObserver.player = self._player
-                self.applyModifiers()
-                self._player?.actionAtItemEnd = .none
-                
-                if #available(iOS 10.0, *) {
-                    self.setAutomaticallyWaitsToMinimizeStalling(self._automaticallyWaitsToMinimizeStalling)
-                }
-                
-                //Perform on next run loop, otherwise onVideoLoadStart is nil
-                self.onVideoLoadStart?([
-                    "src": [
-                        "uri": self._source?.uri ?? NSNull(),
-                        "type": self._source?.type ?? NSNull(),
-                        "isNetwork": NSNumber(value: self._source?.isNetwork ?? false)
-                    ],
-                    "drm": self._drm?.json ?? NSNull(),
-                    "target": self.reactTag
-                ])
-            }.catch{_ in }
+            // perform on next run loop, otherwise other passed react-props may not be set
+            RCTVideoUtils.delay()
+                .then{ [weak self] in
+                    guard let self = self else {throw NSError(domain: "", code: 0, userInfo: nil)}
+                    guard let source = self._source,
+                    let assetResult = RCTVideoUtils.prepareAsset(source: source),
+                    let asset = assetResult.asset,
+                    let assetOptions = assetResult.assetOptions else {
+                        DebugLog("Could not find video URL in source '\(self._source)'")
+                        throw NSError(domain: "", code: 0, userInfo: nil)
+                    }
+
+    #if canImport(RCTVideoCache)
+                    if self._videoCache.shouldCache(source:source, textTracks:self._textTracks) {
+                        return self._videoCache.playerItemForSourceUsingCache(uri: source.uri, assetOptions:assetOptions)
+                    }
+    #endif
+
+                    if self._drm != nil || self._localSourceEncryptionKeyScheme != nil {
+                        self._resouceLoaderDelegate = RCTResourceLoaderDelegate(
+                            asset: asset,
+                            drm: self._drm,
+                            localSourceEncryptionKeyScheme: self._localSourceEncryptionKeyScheme,
+                            onVideoError: self.onVideoError,
+                            onGetLicense: self.onGetLicense,
+                            reactTag: self.reactTag
+                        )
+                    }
+                    return Promise{self.playerItemPrepareText(asset: asset, assetOptions:assetOptions)}
+                }.then{[weak self] (playerItem:AVPlayerItem!) in
+                    guard let self = self else {throw  NSError(domain: "", code: 0, userInfo: nil)}
+                    
+                    self._player?.pause()
+                    self._playerItem = playerItem
+                    self._playerObserver.playerItem = self._playerItem
+                    self.setPreferredForwardBufferDuration(self._preferredForwardBufferDuration)
+                    self.setFilter(self._filterName)
+                    if let maxBitRate = self._maxBitRate {
+                        self._playerItem?.preferredPeakBitRate = Double(maxBitRate)
+                    }
+
+                    self._player = self._player ?? AVPlayer()
+                    self._player?.replaceCurrentItem(with: playerItem)
+                    self._playerObserver.player = self._player
+                    self.applyModifiers()
+                    self._player?.actionAtItemEnd = .none
+
+                    if #available(iOS 10.0, *) {
+                        self.setAutomaticallyWaitsToMinimizeStalling(self._automaticallyWaitsToMinimizeStalling)
+                    }
+
+                    //Perform on next run loop, otherwise onVideoLoadStart is nil
+                    self.onVideoLoadStart?([
+                        "src": [
+                            "uri": self._source?.uri ?? NSNull(),
+                            "type": self._source?.type ?? NSNull(),
+                            "isNetwork": NSNumber(value: self._source?.isNetwork ?? false)
+                        ],
+                        "drm": self._drm?.json ?? NSNull(),
+                        "target": self.reactTag
+                    ])
+                }.catch{_ in }
+        }
         _videoLoadStarted = true
     }
     
