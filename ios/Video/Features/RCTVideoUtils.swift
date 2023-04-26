632e69447bc4c68e21789216475f9504a2ebacde
Igor Tironi <tironiigor@gmail.com>
2023-04-26 15:36:41 -0700

    Request asset precise duration and timing

--- ios/Video/Features/RCTVideoUtils.swift
+++ ios/Video/Features/RCTVideoUtils.swift
@@ -6,7 +6,6 @@ import Photos
  * Collection of pure functions
  */
 enum RCTVideoUtils {
-    
     /*!
      * Calculates and returns the playable duration of the current player item using its loaded time ranges.
      *
@@ -18,7 +17,7 @@ enum RCTVideoUtils {
               video.status == AVPlayerItem.Status.readyToPlay else {
             return 0
         }
-        
+
         var effectiveTimeRange:CMTimeRange?
         for (_, value) in video.loadedTimeRanges.enumerated() {
             let timeRange:CMTimeRange = value.timeRangeValue
@@ -27,14 +26,14 @@ enum RCTVideoUtils {
                 break
             }
         }
-        
+
         if let effectiveTimeRange = effectiveTimeRange {
             let playableDuration:Float64 = CMTimeGetSeconds(CMTimeRangeGetEnd(effectiveTimeRange))
             if playableDuration > 0 {
                 return playableDuration as NSNumber
             }
         }
-        
+
         return 0
     }
 
@@ -42,7 +41,7 @@ enum RCTVideoUtils {
         if filepath.contains("file://") {
             return NSURL(string: filepath as String)
         }
-        
+
         // if no file found, check if the file exists in the Document directory
         let paths:[String]! = NSSearchPathForDirectoriesInDomains(searchPath, .userDomainMask, true)
         var relativeFilePath:String! = filepath.lastPathComponent
@@ -52,33 +51,33 @@ enum RCTVideoUtils {
         if fileComponents.count > 1 {
             relativeFilePath = fileComponents[1]
         }
-        
+
         let path:String! = (paths.first! as NSString).appendingPathComponent(relativeFilePath)
         if FileManager.default.fileExists(atPath: path) {
             return NSURL.fileURL(withPath: path) as NSURL
         }
         return nil
     }
-    
+
     static func playerItemSeekableTimeRange(_ player:AVPlayer?) -> CMTimeRange {
         if let playerItem = player?.currentItem,
            playerItem.status == .readyToPlay,
            let firstItem = playerItem.seekableTimeRanges.first {
             return firstItem.timeRangeValue
         }
-        
+
         return (CMTimeRange.zero)
     }
-    
+
     static func playerItemDuration(_ player:AVPlayer?) -> CMTime {
         if let playerItem = player?.currentItem,
            playerItem.status == .readyToPlay {
             return(playerItem.duration)
         }
-        
+
         return(CMTime.invalid)
     }
-    
+
     static func calculateSeekableDuration(_ player:AVPlayer?) -> NSNumber {
         let timeRange:CMTimeRange = RCTVideoUtils.playerItemSeekableTimeRange(player)
         if CMTIME_IS_NUMERIC(timeRange.duration)
@@ -87,7 +86,7 @@ enum RCTVideoUtils {
         }
         return 0
     }
-    
+
     static func getAudioTrackInfo(_ player:AVPlayer?) -> [AnyObject]! {
         guard let player = player else {
             return []
@@ -116,7 +115,7 @@ enum RCTVideoUtils {
         }
         return audioTracks as [AnyObject]?
     }
-    
+
     static func getTextTrackInfo(_ player:AVPlayer?) -> [TextTrack]! {
         guard let player = player else {
             return []
@@ -145,12 +144,12 @@ enum RCTVideoUtils {
         }
         return textTracks
     }
-    
+
     // UNUSED
     static func getCurrentTime(playerItem:AVPlayerItem?) -> Float {
         return Float(CMTimeGetSeconds(playerItem?.currentTime() ?? .zero))
     }
-    
+
     static func base64DataFromBase64String(base64String:String?) -> Data? {
         if let base64String = base64String {
             return Data(base64Encoded:base64String)
@@ -171,10 +170,10 @@ enum RCTVideoUtils {
 
         return Data(base64Encoded: adoptURL.absoluteString)
     }
-    
+
     static func generateMixComposition(_ asset:AVAsset) -> AVMutableComposition {
         let mixComposition:AVMutableComposition = AVMutableComposition()
-        
+
         let videoAsset:AVAssetTrack! = asset.tracks(withMediaType: AVMediaType.video).first
         let videoCompTrack:AVMutableCompositionTrack! = mixComposition.addMutableTrack(withMediaType: AVMediaType.video, preferredTrackID:kCMPersistentTrackID_Invalid)
         do {
@@ -184,7 +183,7 @@ enum RCTVideoUtils {
                 at: .zero)
         } catch {
         }
-        
+
         let audioAsset:AVAssetTrack! = asset.tracks(withMediaType: AVMediaType.audio).first
         let audioCompTrack:AVMutableCompositionTrack! = mixComposition.addMutableTrack(withMediaType: AVMediaType.audio, preferredTrackID:kCMPersistentTrackID_Invalid)
         do {
@@ -194,14 +193,14 @@ enum RCTVideoUtils {
                 at: .zero)
         } catch {
         }
-        
+
         return mixComposition
     }
-    
+
     static func getValidTextTracks(asset:AVAsset, assetOptions:NSDictionary?, mixComposition:AVMutableComposition, textTracks:[TextTrack]?) -> [TextTrack] {
         let videoAsset:AVAssetTrack! = asset.tracks(withMediaType: AVMediaType.video).first
         var validTextTracks:[TextTrack] = []
-        
+
         if let textTracks = textTracks, textTracks.count > 0 {
             for i in 0..<textTracks.count {
                 var textURLAsset:AVURLAsset!
@@ -227,12 +226,12 @@ enum RCTVideoUtils {
                 }
             }
         }
-        
+
         let emptyVttFile:TextTrack? = self.createEmptyVttFile()
         if (emptyVttFile != nil) {
             validTextTracks.append(emptyVttFile!)
         }
-        
+
         return validTextTracks
     }
 
@@ -245,7 +244,7 @@ enum RCTVideoUtils {
         let fileManager = FileManager.default
         let cachesDirectoryUrl = fileManager.urls(for: .cachesDirectory, in: .userDomainMask)[0]
         let filePath = cachesDirectoryUrl.appendingPathComponent("empty.vtt").path
-        
+
         if !fileManager.fileExists(atPath: filePath) {
             let stringToWrite = "WEBVTT\n\n1\n99:59:59.000 --> 99:59:59.001\n."
 
@@ -255,7 +254,7 @@ enum RCTVideoUtils {
                 return nil
             }
         }
-        
+
         return TextTrack([
             "language": "disabled",
             "title": "EmptyVttFile",
@@ -263,7 +262,7 @@ enum RCTVideoUtils {
             "uri": filePath,
         ])
     }
-    
+
     static func delay(seconds: Int = 0) -> Promise<Void> {
         return Promise<Void>(on: .global()) { fulfill, reject in
             DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(seconds)) / Double(NSEC_PER_SEC), execute: {
@@ -271,7 +270,7 @@ enum RCTVideoUtils {
             })
         }
     }
-    
+
     static func preparePHAsset(uri: String) -> Promise<AVAsset?> {
         return Promise<AVAsset?>(on: .global()) { fulfill, reject in
             let assetId = String(uri[uri.index(uri.startIndex, offsetBy: "ph://".count)...])
@@ -286,26 +285,31 @@ enum RCTVideoUtils {
             }
         }
     }
-    
+
     static func prepareAsset(source:VideoSource) -> (asset:AVURLAsset?, assetOptions:NSMutableDictionary?)? {
         guard let sourceUri = source.uri, sourceUri != "" else { return nil }
         var asset:AVURLAsset!
         let bundlePath = Bundle.main.path(forResource: source.uri, ofType: source.type) ?? ""
         let url = source.isNetwork || source.isAsset
-        ? URL(string: source.uri ?? "")
-        : URL(fileURLWithPath: bundlePath)
-        let assetOptions:NSMutableDictionary! = NSMutableDictionary()
-        
+            ? URL(string: source.uri ?? "")
+            : URL(fileURLWithPath: bundlePath)
+        let assetOptions: NSMutableDictionary! = NSMutableDictionary()
+
+        assetOptions.setValue(true, forKey: "AVURLAssetPreferPreciseDurationAndTimingKey")
+
         if source.isNetwork {
             if let headers = source.requestHeaders, headers.count > 0 {
                 assetOptions.setObject(headers, forKey:"AVURLAssetHTTPHeaderFieldsKey" as NSCopying)
             }
+
             let cookies:[AnyObject]! = HTTPCookieStorage.shared.cookies
+
             assetOptions.setObject(cookies, forKey:AVURLAssetHTTPCookiesKey as NSCopying)
             asset = AVURLAsset(url: url!, options:assetOptions as! [String : Any])
         } else {
-            asset = AVURLAsset(url: url!)
+            asset = AVURLAsset(url: url!, options:assetOptions as! [String : Any])
         }
+
         return (asset, assetOptions)
     }
 }
